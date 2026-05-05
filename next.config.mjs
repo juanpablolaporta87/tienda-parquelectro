a644478c3304c32f1d3a362bc0fbc3777c2bd832
juanpablolaporta87 <juanpablolaporta87@gmail.com>
2026-05-04 23:35:05 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
-};
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
