e3a54a728dbacab1f845a9e49be0af2cc85d5175
Juan Pablo <juanpablolaporta87@gmail.com>
2026-05-11 01:38:54 -0300

    permitir imágenes de parquelectro.com en Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "parquelectro.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.hostingersite.com",
+      },
+    ],
   },
 };
 
